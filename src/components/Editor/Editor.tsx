09d97754e9404b3c625a2c5df7bb558785764eb2
Judicael <46365844+judicaelandria@users.noreply.github.com>
2023-03-16 11:35:19 +0300

    feat(store): sync user data to store

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,11 +1,29 @@
-import { Canvas, TldrawEditor } from "@tldraw/tldraw";
+import {
+  Canvas,
+  getUserData,
+  TldrawEditor,
+  TLInstance,
+  useLocalSyncClient,
+} from "@tldraw/tldraw";
 import "@tldraw/tldraw/editor.css";
 import { PrimaryTool, TopPanel, ZoomMenu } from "~/components";
 
+const instanceId = TLInstance.createCustomId("tl-miro");
+
 export const Editor = () => {
+  const userData = getUserData();
+  const syncedStore = useLocalSyncClient({
+    instanceId,
+    userId: userData.id,
+    universalPersistenceKey: "tl-miro",
+  });
   return (
     <div className="fixed w-full h-full">
-      <TldrawEditor>
+      <TldrawEditor
+        instanceId={instanceId}
+        userId={userData.id}
+        store={syncedStore}
+      >
         <Canvas />
         <TopPanel />
         <PrimaryTool />
